813ca6fe673ba680df7d470b80293fb0bb57999c
Reese Stowell <Reese2as@gmail.com>
2021-11-29 16:45:43 -0500

    first commit

---
+++ Main.cpp
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Reese Stowell
+ */
+#include <iostream>
+#include "std_lib_facilities.h"
+
+int main()
+{
+	string operation;
+	double num = 0;
+	double num2 = 0;
+	cout << "Please enter an operation +, -, *, /, plus, minus, mul, div followed by two values";
+	
+	while(cin >> operation >> num >> num2) {			
+		double result = 0;
+		if(operation=="plus" || operation=="+")
+        { 
+            result = num + num2;
+        }
+		else if (operation=="minus" || operation=="-")
+        {
+        result = num - num2;
+        }
+		else if (operation=="mul" || operation=="*")
+        {
+        result = num * num2;
+        }
+		else if (operation=="div" || operation=="/") 
+        {
+			if (num2==0) error("cant divide by zero");
+			result = num/num2;
+		}
+		cout << num << operation << num2 << " == " << result <<'\n';
+		cout << "Please enter a new opperation ";
+	}
+}
